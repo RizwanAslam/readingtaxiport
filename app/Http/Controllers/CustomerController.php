1f99f20976dc4013b0c3d49536f79311ca83c1e2
sadia721 <58290159+sadia721@users.noreply.github.com>
2019-12-03 06:18:45 -0800

    Update CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -54,7 +54,7 @@ class CustomerController extends Controller
         if($create){
             $data='sadia is great';
             Mail::send('emails.mail',$data,function($message){
-                $message->to('sadiaarooj111@gmail.com','Sadia Arooj')
+                $message->to('sadiaarooj721@gmail.com','Sadia Arooj')
                 ->subject('testing');
                 $message->from('sadiaarooj111@gmail.com','Sadia Arooj');
             });
