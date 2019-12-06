61f6470ab161ad35c2ddd1e047c5618f8b1a0818
sadia721 <58290159+sadia721@users.noreply.github.com>
2019-12-06 04:11:36 -0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,8 +30,13 @@
     $(function() {
         // add input listeners
         google.maps.event.addDomListener(window, 'load', function () {
-            var from_places = new google.maps.places.Autocomplete(document.getElementById('from_places'));
-            var to_places = new google.maps.places.Autocomplete(document.getElementById('to_places'));
+            var defaultBounds = new google.maps.LatLngBounds(
+                new google.maps.LatLng(51.509865,-0.118092));
+             var options = {
+                 bounds: defaultBounds
+            };
+            var from_places = new google.maps.places.Autocomplete(document.getElementById('from_places'),options);
+            var to_places = new google.maps.places.Autocomplete(document.getElementById('to_places'),options);
 
             google.maps.event.addListener(from_places, 'place_changed', function () {
                 var from_place = from_places.getPlace();
