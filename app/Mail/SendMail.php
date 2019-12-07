bddecef4ff27f7388e645fabcbc11e063ff4fc91
sadia721 <58290159+sadia721@users.noreply.github.com>
2019-12-07 01:34:32 -0800

    Create SendMail.php

---
+++ app/Mail/SendMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+use App\customer;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $customer;
+    public function __construct(customer $customer)
+    {
+        //
+        $this->customer = $customer;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('sadiaarooj111@gmail.com')->view('view.mail');
+    }
+}
