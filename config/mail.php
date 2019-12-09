7f0047aba62a7cf04246d567692106b3420827e2
sadia721 <58290159+sadia721@users.noreply.github.com>
2019-12-09 03:49:02 -0800

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'booking@jubilecars.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'bookings@jubilecars.com'),
         'name' => env('MAIL_FROM_NAME', 'jubilecars'),
     ],
 
