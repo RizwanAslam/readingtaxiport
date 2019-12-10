a52309dc2ec67f60b6ded1f11690c3dc9b2c3b52
sadia721 <58290159+sadia721@users.noreply.github.com>
2019-12-10 07:33:14 -0800

    Create receive.blade.php

---
+++ resources/views/emails/receive.blade.php
@@ -0,0 +1,39 @@
+@component('mail::message')
+# Customer hase Placed booking
+## Name of customer
+             {{$customer->name}}
+## Company
+            {{$customer->company}}
+## Email
+            {{$customer->email}}
+## Phone Number
+            {{$customer->telephone}}
+## Pickup Point
+            {{$location->origin}}
+## Destination
+            {{$location->destination}}
+## Distance
+            {{$location->distance}}
+## vehical Type
+             {{$booking->vehicalType}}
+## vehical Type
+             {{$booking->occupants}}
+## vehical Type
+             {{$booking->luggage}}
+## vehical Type
+              {{$booking->suitcase}}
+## vehical Type
+              {{$booking->journey}}
+## vehical Type
+                {{$booking->pickup_time}}
+## vehical Type
+                {{$booking->cost}}£
+## Flight information
+                {{$customer->flightinfo}}
+## Meet and Greet 
+                {{$customer->meetgreet}}
+## Aditional information about flight 
+                {{$customer->aditionalinfo}}
+
+Thanks,<br>
+@endcomponent
